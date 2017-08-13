1ba68553384cbee77e30e144f693ef6bdea8a420
Evan <evanjfrancis@gmail.com>
2017-08-12 22:40:43 -0700

    remove logs for 0.0.3

--- startup.js
+++ startup.js
@@ -1,13 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 
 if (Meteor.isClient) {
-  console.log('efrancis:iview startup isClient');
   import Vue from 'vue';
   import iView from './iview/src/index.js';
 
   Meteor.startup(() => {
     Vue.use(iView);
   });
-} else {
-  console.log('efrancis:iview startup isServer');
 }
