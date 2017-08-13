5c7f6be01a4806874887ef192e0fa56d6411305b
Evan <evanjfrancis@gmail.com>
2017-08-13 10:32:38 -0700

    remove isclient check since specified in package.js

--- startup.js
+++ startup.js
@@ -1,7 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-
-if (Meteor.isClient) {
-  import Vue from 'vue';
-  import iView from './iview/src/index.js';
-  Vue.use(iView);
-}
+import Vue from 'vue';
+import iView from './iview/src/index.js';
+Vue.use(iView);
